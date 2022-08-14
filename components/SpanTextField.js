58b22aad858f005ce7f154ab92ccdb05445094c6
HenHimA1 <hendrahima1@gmail.com>
2022-08-14 21:54:43 +0700

    update

--- components/SpanTextField.js
+++ components/SpanTextField.js
@@ -1,21 +1,28 @@
 import Link from "next/link";
 
 export default function SpanTextField({ label, value, href }) {
+  let valueLabel;
+  
+  if (href) {
+    valueLabel = (
+      <Link href={href}>
+        <a className="pb-2 mb-2 border-b-2 border-white focus:outline-none h-8 underline">
+          {value}
+        </a>
+      </Link>
+    );
+  } else {
+    valueLabel = (
+      <span className="pb-2 mb-2 border-b-2 border-white focus:outline-none h-8">
+        {value}
+      </span>
+    );
+  }
+
   return (
     <div className="grid grid-cols-2">
       <label>{label}</label>
-
-      {href ? (
-        <Link href={href}>
-          <a className="pb-2 mb-2 border-b-2 border-white focus:outline-none h-8 underline">
-            {value}
-          </a>
-        </Link>
-      ) : (
-        <span className="pb-2 mb-2 border-b-2 border-white focus:outline-none h-8">
-          {value}
-        </span>
-      )}
+      {valueLabel}
     </div>
   );
 }
